70574bdbaafc9eb3ea74eb4d6711ba7c592bd9dd
TheLuaFox86 <peanuts005@outlook.com>
2025-02-24 20:17:55 -0500

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -2,10 +2,10 @@
 _G.repo = "https://raw.githubusercontent.com/TheLuaFox86/TerminalFox-Launcher/main/"
 _G.TLFR = "https://raw.githubusercontent.com/TheLuaFox86/TerminalFox-Roblox/main/"
 _G.wget = function(url)
-  return game:GetService("HttpService"):GetAsync(url)
+  return game:GetService("HttpService"):GetAsync(url, true)
 end
 local player = owner or game.Players.LocalPlayer
 local gui = {}
-gui.body = Instance.New("ScreenGui")
+gui.body = Instance.new("ScreenGui")
 gui.body.Parent = player.PlayerGui
-loadstring(wget(TLFR .. "TerminalFox.lua"), true)(player, gui)
+loadstring(wget(TLFR .. "TerminalFox.lua"))(player, gui)
