e1076431e579b2d86ebfcf8485abcadc7a81ba21
TheLuaFox86 <peanuts005@outlook.com>
2025-02-25 18:43:22 -0500

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -3,13 +3,20 @@ if not _G then
   _G = Shared
   luaSandboxMode = true
 end
+local player
+do
+  if luaSandboxMode then
+    player = owner
+  else
+    player = game.Players.LocalPlayer
+  end
+end
 _G.LSM = luaSandboxMode
 _G.repo = "https://raw.githubusercontent.com/TheLuaFox86/TerminalFox-Launcher/main/"
 _G.TLFR = "https://raw.githubusercontent.com/TheLuaFox86/TerminalFox-Roblox/main/"
 _G.wget = function(url)
   return game:GetService("HttpService"):GetAsync(url, true)
 end
-local player = owner or game.Players.LocalPlayer
 local gui = {}
 gui.body = Instance.new("ScreenGui")
 gui.body.Parent = player.PlayerGui
